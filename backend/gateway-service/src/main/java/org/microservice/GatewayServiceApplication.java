32ac53602000c831186a0ada61da844d8fe0aa35
waelamara <wael.benamara@esprit.tn>
2020-10-09 22:43:25 +0100

    creation de proxy-service

--- backend/gateway-service/src/main/java/org/microservice/GatewayServiceApplication.java
+++ backend/gateway-service/src/main/java/org/microservice/GatewayServiceApplication.java
@@ -2,11 +2,13 @@ package org.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
@@ -16,7 +18,7 @@ public class GatewayServiceApplication {
 	@Bean
 	RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
 		return builder.routes()
-				.route(r->r.path("/products/**").uri("http://locallhost:8083/").id("route_1")).build();
+				.route(r->r.path("/products/**").uri("http://localhost:8083/").id("route_1")).build();
 	}
 
 }
