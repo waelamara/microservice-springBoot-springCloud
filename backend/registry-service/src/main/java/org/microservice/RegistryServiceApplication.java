e6d9f93c08231aad762099ba822b8895c9ae397f
waelamara <wael.benamara@esprit.tn>
2020-10-16 12:36:19 +0100

    registry-service

--- backend/registry-service/src/main/java/org/microservice/RegistryServiceApplication.java
+++ backend/registry-service/src/main/java/org/microservice/RegistryServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ProxyServiceApplication {
+public class RegistryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProxyServiceApplication.class, args);
+		SpringApplication.run(RegistryServiceApplication.class, args);
 	}
 
 }
--- backend/registry-service/src/test/java/org/microservice/RegistryServiceApplicationTests.java
+++ backend/registry-service/src/test/java/org/microservice/RegistryServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProxyServiceApplicationTests {
+class RegistryServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
